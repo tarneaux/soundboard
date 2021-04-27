dfc5324fa144b543e8e0fe4e0bd614f8cde1f72f
tarneo <62650051+tarneaux@users.noreply.github.com>
2021-04-27 16:23:40 +0200

    From Tkinter to Gtk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from tkinter import Tk, Button
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk
 import os
 from pygame import mixer
 from functools import partial
@@ -7,33 +9,38 @@ from functools import partial
 class App:
     def __init__(self):
         mixer.init()
-        self.root = Tk()
+        win = Gtk.Window()
+        win.connect("destroy", Gtk.main_quit)
+        self.grid = Gtk.Grid()
+        win.add(self.grid)
         self.buildButtons()
-
         self.gridElements()
-        stopButton = Button(self.root, text="[Stop]", command=mixer.music.stop)
-        xSize, ySize = self.root.grid_size()
-        print(ySize)
-        stopButton.grid(column=3, row=ySize)
-        self.root.mainloop()
-
+        stopButton = Gtk.Button(label="[Stop]")
+        stopButton.connect("clicked", self.stopMusic)
+        self.grid.attach(stopButton, 2, self.height, 1, 1)
+        win.show_all()
+        Gtk.main()
+    def stopMusic(self, junk):
+        mixer.music.stop()
     def buildButtons(self):
         self.elements = []
-        for file in os.listdir("sounds"):
+        for file in os.listdir("."):
             if file.endswith(".mp3"):
-                self.elements.append(Button(self.root, text=file[:-4], command=partial(self.play, file)))
+                self.elements.append(Gtk.Button(label=file[:-4]))
+                self.elements[len(self.elements) - 1].connect("clicked", partial(self.play, file))
     def gridElements(self):
         x = 0
         y = 1
         for b in self.elements:
             x += 1
-            if x == 4:
+            if x == 3:
                 x = 1
                 y += 1
-            b.grid(column=x, row=y)
+            self.grid.attach(b, x, y, 1, 1)
+        self.height = y+1
 
-    def play(self, file):
-        mixer.music.load(os.path.join("sounds", file))
+    def play(self, file, junk):
+        mixer.music.load(os.path.join(".", file))
         mixer.music.play()
 
 App()
